889ce1ad595b4c58b892d3a828b6e06d4c69ffde
Rakibul Islam Biplob <biplob.bd33@gmail.com>
2025-07-23 22:37:43 +0600

    sidebar and modal corrected

--- src/app/pages/transaction/transaction.component.ts
+++ src/app/pages/transaction/transaction.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import * as toastr from 'toastr';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-transaction',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './transaction.component.html',
-  styleUrl: './transaction.component.css'
+  styleUrl: './transaction.component.css',
 })
 export class TransactionComponent {
+  @ViewChild('addExpenseModal') addExpenseModal!: any;
+  modalInstance: any;
 
+  openModal(): void {
+    this.modalInstance = new bootstrap.Modal(
+      this.addExpenseModal.nativeElement
+    );
+    this.modalInstance.show();
+  }
+
+  closeModal(): void {
+    if (this.modalInstance) {
+      this.modalInstance.hide();
+    }
+  }
+
+  submitExpense(): void {
+    toastr.info('Transaction saved successfully.');
+    this.closeModal();
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
