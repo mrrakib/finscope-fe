9cd9018630f125c06bf34122a03f7bf3a365a820
Md. Rakibul Islam Biplob <rakibul.biplob@sslwireless.com>
2025-07-24 12:38:16 +0600

    design changed for transaction add page, managed expense and income same page

--- src/app/pages/transaction/transaction.component.ts
+++ src/app/pages/transaction/transaction.component.ts
@@ -2,19 +2,21 @@ import { Component, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import * as toastr from 'toastr';
 import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 declare var bootstrap: any;
 
 @Component({
   selector: 'app-transaction',
   standalone: true,
-  imports: [FormsModule, NgbDatepickerModule],
+  imports: [FormsModule, NgbDatepickerModule, CommonModule],
   templateUrl: './transaction.component.html',
   styleUrl: './transaction.component.css',
 })
 export class TransactionComponent {
   @ViewChild('addExpenseModal') addExpenseModal!: any;
   modalInstance: any;
+  transactionType: 'Expense' | 'Income' = 'Expense';
 
   openModal(): void {
     this.modalInstance = new bootstrap.Modal(
@@ -27,6 +29,10 @@ export class TransactionComponent {
     if (this.modalInstance) {
       this.modalInstance.hide();
     }
+  }
+
+  toogleTransactionType(type: 'Expense' | 'Income'): void {
+    this.transactionType = type;
   }
 
   submitExpense(): void {
